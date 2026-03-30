91f68800879c9bc5f3ce2d62895079689337d2ec
25f3002130 <25f3002130@ds.study.iitm.ac.in>
2026-03-30 12:44:59 +0530

    Add GET route for Discord API response

---
+++ app/api/.well-known/discord/route.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+  return new Response(
+    'dh=f7036e426cbaa874e5db70e699e8d91a89d25b12',
+    {
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    }
+  );
+}
